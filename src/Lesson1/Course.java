79a246e61449bbee25d6f6a1018923cea52c176a
KonstantinShkarin <shkarinkostya@mail.ru>
2018-12-22 10:39:54 +0300

    1

--- src/Lesson1/Course.java
+++ src/Lesson1/Course.java
@@ -1,4 +1,9 @@
 package Lesson1;
 
+import Lesson1.Obstracles.Cross;
+import Lesson1.Obstracles.Obstracle;
+import Lesson1.Obstracles.Wall;
+import Lesson1.Obstracles.Water;
+
 public class Course {
 }
