5e4f22b38f466ad3dff4ad8748d2c3ddb09cf824
KonstantinShkarin <shkarinkostya@mail.ru>
2019-01-20 12:39:00 +0300

    Lesson 6

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,3 +1,7 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -5,36 +9,59 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    private ServerSocket server;
-    private Socket socket;
-   // PrintWriter writer;
+    private ServerSocket server = null;
+    private Socket socket = null;
+    private DataInputStream in;
+    private DataOutputStream out;
+    // PrintWriter writer;
 
-    public Server(){
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    public Server() {
         try {
             server = new ServerSocket(5000);
             System.out.println("Сервер запущен!");
 
-            socket = new Socket();
+            socket = server.accept();
+            System.out.println("Клиент подключился!");
+
 //            Scanner in = new Scanner(socket.getInputStream());
+            Scanner outS = new Scanner(System.in);
+//
 //            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
 
-            while (true) {
-                socket = server.accept();
-                System.out.println("Клиент подключился!");
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
+            while (true) {
+                String str = in.readUTF();
+                String strO = outS.nextLine();
+                if (str.equals("/end")) {
+                    out.writeUTF("/end");
+                    break;
+                }
+                System.out.println(str);
+                out.writeUTF(strO);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                server.close();
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-         ;
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-        }
-
     }
+}
+
+//
+//
